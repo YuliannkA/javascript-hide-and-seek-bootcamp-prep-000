0eb60be58d4f5329001cfab44fb93258eccb5372
Yulia Drozdova <yuliannka@gmail.com>
2018-01-20 05:08:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ function nestedTarget() {
 function deepestChild() {
  var grandNode = document.querySelecto('#grand-node');
  var deepestNode = grandNode.children[0];
- for (var i = 0; )
-
+ for (var i = 0; i<grandNode.children[i], i++) {
+   deepestNode = grandNode.children[0]
+ }
+return deepestNode
 }
